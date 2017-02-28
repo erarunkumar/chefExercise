ed6a22316dfc23f9be5af4bc59b95f4a11617c14
Ankur Gupta <ankurbjr@gmail.com>
2017-02-28 12:08:44 +0530

    updated cookbooks

---
+++ cookbooks/example/metadata.rb
@@ -0,0 +1,6 @@
+name 'example'
+description 'An example cookbook'
+version '1.0.0'
+maintainer 'opstree solution'
+maintainer_email 'ankurbjr@gmail.com'
+
---
+++ cookbooks/starter/metadata.rb
@@ -0,0 +1,5 @@
+name 'starter'
+description 'A basic starter cookbook'
+version '1.0.0'
+maintainer 'opstree solution'
+maintainer_email 'ankurbjr@gmail.com'
